f37cd2f13e8a2a67cb9a0bd4380f1d0129b6714f
overint <emcmahonzx@gmail.com>
2016-09-18 22:02:20 +1000

    indent fix

--- PaypalIPN.php
+++ PaypalIPN.php
@@ -1,15 +1,17 @@
 <?php
 
-class PaypalIPN {
+class PaypalIPN
+{
 
-	private $use_sandbox = false;
+    private $use_sandbox = false;
+
     private $use_local_certs = true;
 
     /*
      * PayPal IPN postback endpoints
      */
 
-	const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
+    const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
     const SANDBOX_VERIFY_URI = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
 
     /*
@@ -25,9 +27,9 @@ class PaypalIPN {
      * should not be enabled in production).
      * @return void
      */
-    public function	useSandbox()
+    public function useSandbox()
     {
-    	$this->use_sandbox = true;
+        $this->use_sandbox = true;
     }
 
 
@@ -35,14 +37,15 @@ class PaypalIPN {
      * Determine endpoint to post the verification data to.
      * @return string
      */
-    public function	getPaypalUri()
+    public function getPaypalUri()
     {
-    	if ($this->use_sandbox) 
-    	{
-    	 	return self::SANDBOX_VERIFY_URI;
-    	} else {
-    		return self::VERIFY_URI;
-    	}
+        if ($this->use_sandbox)
+        {
+            return self::SANDBOX_VERIFY_URI;
+        } else
+        {
+            return self::VERIFY_URI;
+        }
     }
 
 
@@ -54,54 +57,63 @@ class PaypalIPN {
      * @throws Exception
      */
     function verifyIPN()
-	{
-	    if (!count($_POST))
+    {
+        if ( ! count($_POST))
         {
             throw new Exception("Missing POST Data");
         }
 
-		$raw_post_data = file_get_contents('php://input');
-		$raw_post_array = explode('&', $raw_post_data);
-		$myPost = [];
-		foreach ($raw_post_array as $keyval) {
-			$keyval = explode('=', $keyval);
-			if (count($keyval) == 2) {
-				if ($keyval[0] === 'payment_date') {
-					if (substr_count($keyval[1], '+') === 1)
-						$keyval[1] = str_replace('+', '%2B', $keyval[1]);
-				}
-				$myPost[$keyval[0]] = urldecode($keyval[1]);
-			}
-		}
-		$req = 'cmd=_notify-validate';
-		$get_magic_quotes_exists = false;
-		if (function_exists('get_magic_quotes_gpc')) {
-			$get_magic_quotes_exists = true;
-		}
-		foreach ($myPost as $key => $value) {
-			if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-				$value = urlencode(stripslashes($value));
-			} else {
-				$value = urlencode($value);
-			}
-			$req .= "&$key=$value";
-		}
+        $raw_post_data = file_get_contents('php://input');
+        $raw_post_array = explode('&', $raw_post_data);
+        $myPost = [];
+        foreach ($raw_post_array as $keyval)
+        {
+            $keyval = explode('=', $keyval);
+            if (count($keyval) == 2)
+            {
+                if ($keyval[0] === 'payment_date')
+                {
+                    if (substr_count($keyval[1], '+') === 1)
+                    {
+                        $keyval[1] = str_replace('+', '%2B', $keyval[1]);
+                    }
+                }
+                $myPost[$keyval[0]] = urldecode($keyval[1]);
+            }
+        }
+        $req = 'cmd=_notify-validate';
+        $get_magic_quotes_exists = false;
+        if (function_exists('get_magic_quotes_gpc'))
+        {
+            $get_magic_quotes_exists = true;
+        }
+        foreach ($myPost as $key => $value)
+        {
+            if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1)
+            {
+                $value = urlencode(stripslashes($value));
+            } else
+            {
+                $value = urlencode($value);
+            }
+            $req .= "&$key=$value";
+        }
 
-		$ch = curl_init($this->getPaypalUri());
-		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        $ch = curl_init($this->getPaypalUri());
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         if ($this->use_local_certs)
         {
             curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . "/cert/cacert.pem");
         }
-		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Connection: Close']);
-		$res = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [ 'Connection: Close' ]);
+        $res = curl_exec($ch);
         $info = curl_getinfo($ch);
         $http_code = $info['http_code'];
 
@@ -110,19 +122,21 @@ class PaypalIPN {
             throw new Exception("PayPal responded with http code $http_code");
         }
 
-		if (!($res)) {
+        if ( ! ( $res ))
+        {
             $errno = curl_errno($ch);
             $errstr = curl_error($ch);
             curl_close($ch);
             throw new Exception("cURL error: [$errno] $errstr");
-		}
-		curl_close($ch);
-		if ($res == self::VALID)
-		{
-			return true;
-		} else {
-			return false;
-		}
-	}
+        }
+        curl_close($ch);
+        if ($res == self::VALID)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
 }
 
